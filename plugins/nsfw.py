b3eb25447838f758573eead7818f74a438c1e140
troublescope <tomiprs25@gmail.com>
2025-04-01 22:14:00 +0700

    feat: add nsfw plugin with missav downloader
    This commit introduces a new plugin that enables users to download content from MissAV.
    The plugin includes a command handler for the `/missav` command, which takes a MissAV link as input.
    It then processes the link and downloads the video with the lowest quality using the `missav_dl` function from the `core` module.
    A processing message is displayed while the download is in progress.
    
    Signed-off-by: troublescope <tomiprs25@gmail.com>

---
+++ plugins/nsfw.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, filters
+
+from core import missav_dl
+
+
+@Client.on_message(filters.command("missav"))
+async def missav_cmd(client: Client, message):
+    if len(message.command) < 2:
+        return await message.reply("Go to MissAV and copy the link!")
+
+    link = message.command[1]
+
+    process = await message.reply("Processing...")
+    await missav_dl(link, quality="lowest", msg=process)
